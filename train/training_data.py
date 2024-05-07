b12bd62df61bcc1b117ea23774f8d2ae6d5edae9
Louis Viglietta <louisv@princeton.edu>
2024-05-06 23:52:13 -0400

    training visualization

--- train/training_data.py
+++ train/training_data.py
@@ -23,15 +23,20 @@ if __name__ == "__main__":
     training_loss_checkpoints = list(range(0, 500*len(training_losses), 500))
     validation_checkpoints = list(range(2000, 2000*(len(validation_losses)+1), 2000))
 
+    plt.subplot(121)
     plt.plot(training_loss_checkpoints[1:], training_losses[1:], "r-")
+    plt.title("Training Loss")
+    plt.xlabel("Training iteration")
+    plt.ylabel("Mean squared error")
+    plt.subplot(122)
     plt.plot(validation_checkpoints, validation_losses, 'b-')
-    plt.legend(["Training", "Validation"])
+    plt.title("Validation Loss")
     plt.xlabel("Training iteration")
     plt.ylabel("Mean squared error")
     plt.savefig('foo.pdf')
-
     plt.close()
+
     plt.plot(validation_checkpoints, corrs, 'g-')
     plt.xlabel("Training iteration")
-    plt.ylabel("Correlation")
+    plt.ylabel("Average correlation")
     plt.savefig('bar.pdf')
